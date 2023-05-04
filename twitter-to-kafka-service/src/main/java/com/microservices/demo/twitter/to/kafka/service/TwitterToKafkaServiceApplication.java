798f7024d949bc2d58c36d712e4df8c07bca8d7d
Johann Yucra Navarrete <jyucrana@emeal.nttdata.com>
2023-05-04 12:11:32 -0400

    twitter4j added

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -3,6 +3,7 @@ package com.microservices.demo.twitter.to.kafka.service;
 import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,11 +13,10 @@ import java.util.Arrays;
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
     private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
-    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
 
-    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData) {
-        this.twitterToKafkaServiceConfigData = configData;
-    }
+    @Autowired
+    private TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
     }
